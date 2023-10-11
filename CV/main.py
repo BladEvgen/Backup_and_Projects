07c795131b2e2d6b24829c9bf6340c5b9e18221a
Evgeniy <ekozlov00@mail.ru>
2023-10-11 13:24:32 +0600

    Created PyQt Folder With interface programs

--- CV/main.py
+++ CV/main.py
@@ -131,23 +131,27 @@ class OpenCv:
         def detect_faces_video(video_path: str):
             speed: float = 1.0
             cap: cv2.VideoCapture = cv2.VideoCapture(video_path)
-            face_cascade = cv2.CascadeClassifier('data\haarcascade_frontalface_default.xml')
+            face_cascade = cv2.CascadeClassifier(
+                "data\haarcascade_frontalface_default.xml"
+            )
 
             while cap.isOpened():
                 ret, frame = cap.read()
                 if not ret:
                     break
-                
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=4)
 
-                for (x, y, w, h) in faces:
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                faces = face_cascade.detectMultiScale(
+                    gray, scaleFactor=1.3, minNeighbors=4
+                )
+
+                for x, y, w, h in faces:
                     cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-                cv2.imshow('Video', frame)
-                if cv2.waitKey(int(1000 / (speed * 24))) & 0xFF == ord('q'):
+                cv2.imshow("Video", frame)
+                if cv2.waitKey(int(1000 / (speed * 24))) & 0xFF == ord("q"):
                     break
-                
+
             cap.release()
             cv2.destroyAllWindows()
 
