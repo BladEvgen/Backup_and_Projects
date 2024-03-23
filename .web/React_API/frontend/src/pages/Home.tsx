7f56eeb2e6d81a191464e9932dfda23688885401
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-03-23 17:33:01 +0500

    update

---
+++ .web/React_API/frontend/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import App from "..";
+import * as bases from "../components/bases";
+
+export default function Page() {
+  return (
+    <bases.Base2>
+      <p className="text-warning">HOME PAGE</p>
+      <div className={"text-white "}>
+        <App />
+      </div>
+    </bases.Base2>
+  );
+}
