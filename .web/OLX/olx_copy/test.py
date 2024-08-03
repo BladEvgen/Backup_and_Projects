83f6f684db535ffa6672e01af382b5e1095e1c61
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-08-03 21:58:21 +0300

    Update test.py

--- .web/OLX/olx_copy/test.py
+++ .web/OLX/olx_copy/test.py
@@ -12,7 +12,6 @@ from monitoring_app import models
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
-DAYS = 1
 
 DB_PATH = Path(__file__).resolve().parent.parent / "db.sqlite3"
 LOGS_PATH = Path(__file__).resolve().parent.parent / "log"
@@ -20,89 +19,6 @@ LOGS_PATH = Path(__file__).resolve().parent.parent / "log"
 DB_PATH.parent.mkdir(parents=True, exist_ok=True)
 LOGS_PATH.mkdir(parents=True, exist_ok=True)
 
-
-def get_attendance_data(pin: str):
-    """
-    Получает данные о посещаемости сотрудника за предыдущий день.
-
-    Args:
-        pin (str): Идентификационный номер сотрудника (PIN-код).
-
-    Returns:
-        list: Список словарей, содержащих информацию о каждом проходе
-              сотрудника через систему контроля доступа за указанный день.
-              Если произошла ошибка, возвращает пустой список.
-    """
-    prev_date = datetime.datetime.now() - datetime.timedelta(days=DAYS)
-
-    eventTime_first = prev_date.strftime("%Y-%m-%d 00:00:00")
-    eventTime_last = prev_date.strftime("%Y-%m-%d 23:59:59")
-    access_token = settings.API_KEY
-
-    try:
-        params = {
-            "endDate": eventTime_last,
-            "pageNo": "1",
-            "pageSize": "1000",
-            "personPin": pin,
-            "startDate": eventTime_first,
-            "access_token": access_token,
-        }
-        response = requests.get(
-            settings.API_URL_LOCAL + "/api/transaction/listAttTransaction",
-            params=params,
-            verify=False,
-        )
-        response.raise_for_status()
-        response_json = response.json()
-        return response_json.get("data", [])
-    except Exception as e:
-        print(f"Error, Pin: {pin}, Error: {e}")
-        return []
-
-
-def get_all_attendance():
-    """
-    Получает данные о посещаемости всех сотрудников за текущий день
-    и сохраняет их в базе данных.
-
-    Выполняет запрос данных о посещаемости для каждого сотрудника
-    параллельно с помощью ThreadPoolExecutor. Затем для каждого
-    сотрудника извлекает первое и последнее время прохода
-    (если данные о посещаемости имеются) и сохраняет их
-    в модели StaffAttendance.
-
-    Returns:
-        None
-    """
-    pins = models.Staff.objects.values_list("pin", flat=True)
-    attendance_data = {}
-
-    with ThreadPoolExecutor(max_workers=6) as executor:
-        for pin, data in zip(pins, executor.map(get_attendance_data, pins)):
-            attendance_data[pin] = data
-
-    with transaction.atomic():
-        for pin, data in attendance_data.items():
-            staff = models.Staff.objects.get(pin=pin)
-            if data:
-                first_event_time = data[-1]["eventTime"]
-                last_event_time = (
-                    data[0]["eventTime"] if len(data) > 1 else first_event_time
-                )
-            else:
-                first_event_time = None
-                last_event_time = None
-            prev_date = datetime.datetime.now() - datetime.timedelta(days=DAYS)
-            next_day = prev_date + datetime.timedelta(days=1)
-
-            staff_attendance = models.StaffAttendance.objects.create(
-                staff=staff,
-                first_in=first_event_time,
-                last_out=last_event_time,
-                date_at=next_day.date(),
-            )
-            staff_attendance.save()
 
 
 def password_check(password: str) -> bool:
